4ddaa4f3e0fb39189b458decaab106def6a12b37
alkhudariyosef-collab <alkhudariyosef@gmail.com>
2025-10-31 17:10:38 +0300

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+"""
+Machine Learning Models Module
+This module contains ML model implementations and evaluation metrics.
+Developed by: Student 3
+"""
+
+from .ml_models import MLModels
+from .evaluator import ModelEvaluator
+from .metrics import PerformanceMetrics
+
+__all__ = ['MLModels', 'ModelEvaluator', 'PerformanceMetrics']
+
