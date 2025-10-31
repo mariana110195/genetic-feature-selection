a2cf7a8e271965c2154bd17cc297ce66f56ba0af
mariana110195 <marianakim107@gmail.com>
2025-10-31 14:14:49 +0300

    Add files via upload

---
+++ data_processing/__init__.py
@@ -0,0 +1,12 @@
+"""
+Data Processing Module
+This module handles data loading, preprocessing, and validation.
+Developed by: Student 1
+"""
+
+from .loader import DataLoader
+from .preprocessor import DataPreprocessor
+from .validator import DataValidator
+
+__all__ = ['DataLoader', 'DataPreprocessor', 'DataValidator']
+
